7c1433eab56c5a7d0e1d2aebd878c59babca163f
Milan Tair <milan.tair@gmail.com>
2020-05-30 23:01:17 +0200

    Load top level categories on the home page

--- src/index.tsx
+++ src/index.tsx
@@ -20,10 +20,6 @@ const menuItems = [
   new MainMenuItem("Contact", "/contact/"),
   new MainMenuItem("Log in", "/user/login/"),
   new MainMenuItem("Register", "/user/register/"),
-
-  new MainMenuItem("Cat 1", "/category/1/"),
-  new MainMenuItem("Cat 7", "/category/7/"),
-  new MainMenuItem("Cat 21", "/category/21/"),
 ];
 
 ReactDOM.render(
